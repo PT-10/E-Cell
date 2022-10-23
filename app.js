4104cfc4495a1c508a29a7caa584c4bf4cc62605
PT-10 <102211549+PT-10@users.noreply.github.com>
2022-10-23 13:30:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-50px";
-  }
-}
-
 
 ScrollReveal({ reset: true });
 
