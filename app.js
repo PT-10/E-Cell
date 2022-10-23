019515d2752a7e7661e2b5a6f6149c481024424b
PT-10 <102211549+PT-10@users.noreply.github.com>
2022-10-23 12:57:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+}
+
+
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 3000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10
+  }
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 5000,
+  move: 0
+});
+
+ScrollReveal().reveal(".scaleUp", {
+  duration: 4000,
+  scale: 0.85
+});
+
+ScrollReveal().reveal(".flip", {
+  delay: 500,
+  duration: 2000,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
+
+
+var typing=new Typed(".text", {
+  strings: [" ", "Ideate.", "Innovate.", "Inspire."," "],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true,
+});
